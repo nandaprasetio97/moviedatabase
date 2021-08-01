2ed0eb07234636f6be821a470ee12fa9f9cd7a75
Nanda Prasetio <nandaprasesoft98@gmail.com>
2021-08-01 20:33:16 +0700

    Change PagingDataViewModel from open to abstract and remove unused import.

--- core/src/main/java/com/nandaprasetio/moviedatabase/core/viewmodel/PagingDataViewModel.kt
+++ core/src/main/java/com/nandaprasetio/moviedatabase/core/viewmodel/PagingDataViewModel.kt
@@ -1,8 +1,7 @@
 package com.nandaprasetio.moviedatabase.core.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.nandaprasetio.moviedatabase.core.misc.BooleanWrapper
 
-open class PagingDataViewModel: ViewModel() {
+abstract class PagingDataViewModel: ViewModel() {
     var refreshWithSwiping = false
 }
